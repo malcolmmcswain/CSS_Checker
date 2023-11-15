e7906118020ffc02fe391e83c0f5f037bc1b2a23
Malcolm McSwain <mcswain1@pacbell.net>
2023-11-15 13:26:26 -0800

    Add additional checks for CSS and HTML

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ let htmlFileCounter = 0;
 
 async function downloadResource(resourceUrl, folderName, isStylesheet) {
   const resourceName = path.basename(resourceUrl);
-  const resourcePrefix = isStylesheet ? "css" : "html";
+  if (resourceName.includes(".pdf")) return;
   const counter = isStylesheet ? ++styleSheetCounter : ++htmlFileCounter;
-  const resourceFileName = `${folderName}_${counter}_${resourceName}`;
+  const resourceFileName = isStylesheet ? `${folderName}_${counter}_${resourceName}` : `${folderName}_${counter}_${resourceName}.html`;
   const resourceFilePath = path.join(folderName, resourceFileName);
 
   try {
@@ -41,6 +41,8 @@ async function analyzeCSSFiles(folderName) {
     .filter((file) => file.endsWith(".css"))
     .map((file) => path.join(folderName, file));
 
+  console.log('\nCSS ANALYSIS:\n');
+
   for (const filePath of cssFilePaths) {
     const content = await fs.readFile(filePath, "utf8");
     const lines = content.split(/\r?\n/);
@@ -51,35 +53,113 @@ async function analyzeCSSFiles(folderName) {
     let flexOrGridFound = false;
     let cssVariablesFound = false;
     let fallbackFontsFound = false;
+    let relativeUnitsFound = false;
+    let dynamicViewportUnitsFound = false;
+    let animationsFound = false;
+    let transitionsFound = false;
+    let transformsFound = false;
+    let advancedColorFunctionsFound = false;
+    let pseudoClassesFound = false;
+    let mediaQueries = [];
 
     // Check each line for the CSS properties
     lines.forEach((line) => {
       if (!flexOrGridFound && line.match(/display:\s*(flex|grid)/)) {
-        console.log(`- Uses Flexbox or Grid: ${line.trim()}`);
+        console.log(`✓ Uses Flexbox or Grid: \n${line.trim()}`);
         flexOrGridFound = true;
       }
 
-      if (!cssVariablesFound && line.match(/var\(--[\w-]+\)/)) {
-        console.log(`- Uses CSS variables: ${line.trim()}`);
+      if (!cssVariablesFound && line.match(/var\(--/)) {
+        console.log(`✓ Uses CSS variables: \n${line.trim()}`);
         cssVariablesFound = true;
       }
 
       if (!fallbackFontsFound && line.match(/font-family:.*?,/)) {
-        console.log(`- Uses fallback fonts: ${line.trim()}`);
+        console.log(`✓ Uses fallback fonts: \n${line.trim()}`);
         fallbackFontsFound = true;
+      }
+
+      if (!relativeUnitsFound && line.match(/(em|vh|vw|%)|calc\(/)) {
+        console.log(`✓ Uses relative units: \n${line.trim()}`);
+        relativeUnitsFound = true;
+      }
+
+      if (!dynamicViewportUnitsFound && line.match(/dvw|dvh/)) {
+        console.log(`✓ Uses dynamic viewport units: \n${line.trim()}`);
+        dynamicViewportUnitsFound = true;
+      }
+
+      if (!animationsFound && line.match(/@keyframes|animation:/)) {
+        console.log(`✓ Uses animations: \n${line.trim()}`);
+        animationsFound = true;
+      }
+
+      if (!transitionsFound && line.match(/transition:/)) {
+        console.log(`✓ Uses transitions: \n${line.trim()}`);
+        transitionsFound = true;
+      }
+
+      if (!transformsFound && line.match(/transform:/)) {
+        console.log(`✓ Uses transforms: \n${line.trim()}`);
+        transformsFound = true;
+      }
+
+      if (!advancedColorFunctionsFound && line.match(/color\(|color-mix\(/)) {
+        console.log(`✓ Uses advanced color functions: \n${line.trim()}`);
+        advancedColorFunctionsFound = true;
+      }
+
+      if (!pseudoClassesFound && line.match(/:has\(|:is\(|:where\(/)) {
+        console.log(`✓ Uses has(), is(), or where() pseudo-classes: \n${line.trim()}`);
+        pseudoClassesFound = true;
+      }
+
+      if (line.match(/@media/)) {
+        mediaQueries.push(line.trim());
       }
     });
 
     // Check for absence
-    if (!flexOrGridFound) {
-      console.log("- Flexbox or Grid not used.");
-    }
-    if (!cssVariablesFound) {
-      console.log("- CSS variables not used.");
-    }
-    if (!fallbackFontsFound) {
-      console.log("- Fallback fonts not used.");
-    }
+    if (!flexOrGridFound) console.log("✗ Flexbox or Grid not used.");
+    if (!cssVariablesFound) console.log("✗ CSS variables not used.");
+    if (!fallbackFontsFound) console.log("✗ Fallback fonts not used.");
+    if (!relativeUnitsFound) console.log("✗ Relative units not used.");
+    if (!dynamicViewportUnitsFound) console.log("✗ Dynamic viewport units not used.");
+    if (!animationsFound) console.log("✗ Animations not used.");
+    if (!transitionsFound) console.log("✗ Transitions not used.");
+    if (!transformsFound) console.log("✗ Transforms not used.");
+    if (!advancedColorFunctionsFound) console.log("✗ Advanced color functions not used.");
+    if (!pseudoClassesFound) console.log("✗ has(), is(), or where() pseudo-classes not used.");
+
+    console.log("\nMedia queries:");
+    mediaQueries.forEach((query, _) => { console.log(query); })
+  }
+}
+
+async function analyzeHTMLFiles(folderName) {
+  const htmlFiles = await fs.readdir(folderName);
+  const htmlFilePaths = htmlFiles
+    .filter((file) => file.endsWith(".html"))
+    .map((file) => path.join(folderName, file));
+
+  console.log("\n\nHTML ANALYSIS:\n");
+
+  for (const filePath of htmlFilePaths) {
+    const content = await fs.readFile(filePath, "utf8");
+    const lines = content.split(/\r?\n/);
+
+    console.log(`Analyzing ${filePath}:`);
+
+    // Check each line for the HTML elements
+    lines.forEach((line, index) => {
+      if (line.includes('<img')) {
+        console.log(`- <img> tag found on line ${index + 1}: ${line.trim()}`);
+      }
+
+      if (line.includes('<picture')) {
+        console.log(`- <picture> tag found on line ${index + 1}: ${line.trim()}`);
+      }
+    });
   }
 }
 
@@ -148,6 +228,7 @@ async function startCrawl(startUrl) {
 
   // Once all files are downloaded, analyze the CSS files
   await analyzeCSSFiles(folderName);
+  await analyzeHTMLFiles(folderName);
 }
 
 const startUrl = process.argv[2];
